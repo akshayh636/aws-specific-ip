cef268a9d81b4350b045f855f1c0c0e2d0e49711
Akshay Halder <akshaysagarpara96@gmail.com>
2024-10-26 21:22:27 +0530

    Update environment.sh

--- environment.sh
+++ environment.sh
@@ -29,32 +29,24 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Create a directory for virtual environments
-echo "Creating virtual environment directory..."
-mkdir -p ~/akshay &>/dev/null
-if [ $? -ne 0 ]; then
-    echo "Failed to create the environments directory. Exiting..."
-    exit 1
-fi
-
-cd ~/akshay
-if [ $? -ne 0 ]; then
-    echo "Failed to navigate to the environments directory. Exiting..."
-    exit 1
-fi
-
 # Create and start the virtual Python environment
-echo "Starting the virtual Python environment..."
-python3 -m venv akshay &>/dev/null
+echo "Creating the virtual environment directory..."
+# Create the akshay directory in the home folder with root access
+sudo mkdir -p ~/akshay
+# Create the virtual environment inside the akshay folder
+python3 -m venv ~/akshay &>/dev/null
 
 if [ $? -ne 0 ]; then
     echo "Failed to create the virtual environment. Exiting..."
     exit 1
 fi
 
+# Change permissions of the virtual environment folder
+sudo chown -R $(whoami):$(whoami) ~/akshay
+sudo chmod -R 775 ~/akshay
+
 # Provide instructions for the user to manually activate the environment
 echo -e "${GREEN}To activate the virtual environment, run the following commands:${NC}"
-echo -e "${GREEN}cd ~/environments/akshay${NC}"
-echo -e "${GREEN}source bin/activate${NC}"
+echo -e "${GREEN}source ~/akshay/bin/activate${NC}"
 
 echo "Python environment setup is successful."
